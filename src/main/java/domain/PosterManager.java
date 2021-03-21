cb3524e8e74d2488c1acea7a01a5d4b35f34a36a
ILia Berestov <berestov_2000@bk.ru>
2021-03-21 19:40:49 +0300

    The third commit

--- src/main/java/domain/PosterManager.java
+++ src/main/java/domain/PosterManager.java
@@ -1,8 +1,10 @@
 package domain;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class PosterManager {
     private int maxLastFilm = 10;
 
@@ -11,21 +13,6 @@ public class PosterManager {
     }
 
     private PurchaseItem[] items = new PurchaseItem[0];
-
-    public void shouldTakeTenFilms(PurchaseItem item) {
-        PurchaseItem[] heh = new PurchaseItem[items.length];
-        int length = items.length + 1;
-        if (length == maxLastFilm) {
-            for (int i = 0; i < items.length; i++) {
-                heh[i] = items[i];
-            }
-        } else if (length > maxLastFilm) {
-            for (int i = 0; i < maxLastFilm; i++) {
-                heh[i] = items[i];
-            }
-        } else return;
-    }
-
 
     public PurchaseItem[] shouldTakeMaxFilms() {
         int length = items.length + 1;
@@ -36,18 +23,22 @@ public class PosterManager {
                 heh[i] = items[index];
             }
             return heh;
+        } else for (int i = 0; i < maxLastFilm; i++) {
+            int index = items.length - i - 1;
+            heh[i] = items[i];
         }
-        else return null;
+        return heh;
     }
 
 
-    public void add(PurchaseItem item) {
+    public PurchaseItem[] add(PurchaseItem item) {
         int length = items.length + 1;
         PurchaseItem[] tmp = new PurchaseItem[length];
         System.arraycopy(items, 0, tmp, 0, items.length);
         int lastIndex = tmp.length - 1;
         tmp[lastIndex] = item;
         items = tmp;
+        return tmp;
     }
 
 
--- src/test/java/domain/PosterManagerTest.java
+++ src/test/java/domain/PosterManagerTest.java
@@ -9,26 +9,31 @@ class PosterManagerTest {
     @Test
     void shouldTakeMaxFilms() {
         PosterManager manager = new PosterManager(5);
-        PurchaseItem first = new PurchaseItem(1,3,"rivay",10,8);
-        PurchaseItem second = new PurchaseItem(2,3,"rivay2",10,8);
-        PurchaseItem third = new PurchaseItem(3,3,"rivay3",10,8);
-        PurchaseItem fourth = new PurchaseItem(4,3,"rivay4",10,8);
-        PurchaseItem fifth = new PurchaseItem(5,3,"rivay5",10,8);
-        PurchaseItem sixth = new PurchaseItem(6,3,"rivay6",10,8);
+        PurchaseItem first = new PurchaseItem(1, 3, "rivay", 10, 8);
+        PurchaseItem second = new PurchaseItem(2, 3, "rivay2", 10, 8);
+        PurchaseItem third = new PurchaseItem(3, 3, "rivay3", 10, 8);
+        PurchaseItem fourth = new PurchaseItem(4, 3, "rivay4", 10, 8);
+        PurchaseItem fifth = new PurchaseItem(5, 3, "rivay5", 10, 8);
+        PurchaseItem sixth = new PurchaseItem(6, 3, "rivay6", 10, 8);
         manager.add(first);
         manager.add(second);
         manager.add(third);
         manager.add(fourth);
         manager.add(fifth);
         manager.add(sixth);
-        PurchaseItem[] actual =manager.shouldTakeMaxFilms();
-        PurchaseItem[] expected = new PurchaseItem []{first,second,third,fourth,fifth};
+        PurchaseItem[] actual = manager.shouldTakeMaxFilms();
+        PurchaseItem[] expected = new PurchaseItem[]{first, second, third,fourth, fifth};
+        assertArrayEquals(expected, actual);
     }
+
 
     @Test
     void add(PurchaseItem item) {
-        PosterManager poster = new PosterManager(5);
-
+        PosterManager manager = new PosterManager(5);
+        PurchaseItem once = new PurchaseItem(1, 3, "rivay", 10, 8);
+        manager.add(once);
+        PurchaseItem[] actual = manager.add(once);
+        PurchaseItem[] expected = new PurchaseItem[]{once};
+        assertArrayEquals(expected,actual);
     }
-
 }
